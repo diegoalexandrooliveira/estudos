2c95954efa522906f8af9a847a9e4dbe157c6fd3
diego.alexandro <diego.alexandro@SPON190100249.sp01.local>
2019-09-09 13:39:37 -0300

    commit

---
+++ SpringBoot/src/main/java/br/com/diego/estudos/ListaVip/Configuracao.java
@@ -0,0 +1,12 @@
+package br.com.diego.estudos.ListaVip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Configuracao {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Configuracao.class, args);
+	}
+}
