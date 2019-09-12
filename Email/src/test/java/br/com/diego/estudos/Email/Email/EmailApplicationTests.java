4399b7016df82aab934c2063b284cc03e4e71b3b
diego.alexandro <diego.alexandro@host.docker.internal>
2019-09-12 13:53:33 -0300

    commit

---
+++ Email/src/test/java/br/com/diego/estudos/Email/Email/EmailApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.diego.estudos.Email.Email;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EmailApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
