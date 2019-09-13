5f6cc5c2b2e72f5b669bf00b307cc940ba0486c4
diego <diego@diego>
2019-09-12 23:49:07 -0300

    commit

---
+++ RabbitMQ/src/main/java/br/com/diego/estudos/RabbitMQ/Runner.java
@@ -0,0 +1,27 @@
+package br.com.diego.estudos.RabbitMQ;
+
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Runner implements CommandLineRunner {
+
+	private final RabbitTemplate rabbitTemplate;
+	private final Receiver receiver;
+
+	public Runner(RabbitTemplate rabbitTemplate, Receiver receiver) {
+		this.rabbitTemplate = rabbitTemplate;
+		this.receiver = receiver;
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Sending message...");
+		rabbitTemplate.convertAndSend(RabbitMqApplication.topicExchangeName, "foo.bar.baz", "Hello from RabbitMQ!");
+		receiver.getLatch().await(10000, TimeUnit.MILLISECONDS);
+	}
+
+}
