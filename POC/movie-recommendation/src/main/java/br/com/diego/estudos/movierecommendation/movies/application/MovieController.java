5aff06ae45ba479b671342b7761c7a5cfa4de6f8
Diego <diegoalexandrooliveira@gmail.com>
2019-10-22 22:11:05 -0300

    - Enabled CORS

--- POC/movie-recommendation/src/main/java/br/com/diego/estudos/movierecommendation/movies/application/MovieController.java
+++ POC/movie-recommendation/src/main/java/br/com/diego/estudos/movierecommendation/movies/application/MovieController.java
@@ -4,6 +4,7 @@ package br.com.diego.estudos.movierecommendation.movies.application;
 import br.com.diego.estudos.movierecommendation.movies.domain.Movie;
 import br.com.diego.estudos.movierecommendation.movies.domain.MovieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import java.util.List;
 
 @RequestMapping({"/movies"})
 @RestController
+@CrossOrigin
 public class MovieController {
 
     @Autowired
--- POC/movie-recommendation/src/main/java/br/com/diego/estudos/movierecommendation/user/application/UserController.java
+++ POC/movie-recommendation/src/main/java/br/com/diego/estudos/movierecommendation/user/application/UserController.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping({"/users"})
+@CrossOrigin
 public class UserController {
 
     @Autowired
