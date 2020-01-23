fb2e7800767a7ac0dbe726fd6e4b24ab35d87b13
Diego <diegoalexandrooliveira@gmail.com>
2020-01-23 19:57:20 -0300

    Added StrategyPattern

--- Backend/DesignPatterns/src/main/java/behavioral/strategy/AmexStrategy.java
+++ Backend/DesignPatterns/src/main/java/behavioral/strategy/AmexStrategy.java
@@ -3,6 +3,9 @@ package behavioral.strategy;
 public class AmexStrategy implements ValidationStrategy {
     @Override
     public boolean isValid(CreditCard creditCard) {
+        if (creditCard.getNumber().equals("123")) {
+            return true;
+        }
         return false;
     }
 }
--- Backend/DesignPatterns/src/main/java/behavioral/strategy/StrategyDemo.java
+++ Backend/DesignPatterns/src/main/java/behavioral/strategy/StrategyDemo.java
@@ -5,5 +5,10 @@ public class StrategyDemo {
 
     public static void main(String[] args) {
         CreditCard creditCard = new CreditCard(new AmexStrategy());
+
+        creditCard.setCcv("400");
+        creditCard.setNumber("123");
+
+        System.out.println(creditCard.isValid());
     }
 }
