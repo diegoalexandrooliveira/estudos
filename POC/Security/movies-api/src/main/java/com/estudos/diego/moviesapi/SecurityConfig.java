cd897897ebf46237ae4b914a22ce05f0d2d78f50
Diego Alexandro de Oliveira <diegoalexandrooliveira@gmail.com>
2020-02-25 11:27:48 -0300

    Implements OAuth2

--- POC/Security/movies-api/src/main/java/com/estudos/diego/moviesapi/SecurityConfig.java
+++ POC/Security/movies-api/src/main/java/com/estudos/diego/moviesapi/SecurityConfig.java
@@ -1,26 +1,22 @@
 package com.estudos.diego.moviesapi;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("admin")
-                .password(passwordEncoder().encode("admin"))
-                .roles("MANAGER");
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors()
+                .and()
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .oauth2ResourceServer()
+                .jwt();
     }
 }
 
--- POC/Security/movies-api/src/main/java/com/estudos/diego/moviesapi/movies/application/MovieController.java
+++ POC/Security/movies-api/src/main/java/com/estudos/diego/moviesapi/movies/application/MovieController.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping(path = "/api/movies")
+@CrossOrigin
 public class MovieController {
 
     @Autowired
